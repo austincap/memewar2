d037f2fa741be2f5413f4ed1a9856d19759de015
austincap <austin.capobianco@gmail.com>
2024-06-01 12:43:18 -0400

    3d stuff

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 
 const socket = io();
-var postsOnThisPage = []
+var postsOnThisPage = [];
 
 
 // const EC = require('elliptic').ec;
@@ -448,20 +448,25 @@ function openGameView(){
 
 
 
-(function() {
-  var canvas = document.getElementById('basicGlobe');
-  var planet = planetaryjs.planet();
-  // Loading this plugin technically happens automatically,
-  // but we need to specify the path to the `world-110m.json` file.
-  planet.loadPlugin(planetaryjs.plugins.earth({
-    topojson: { file: 'world-110m.json' }
-  }));
-  // Scale the planet's radius to half the canvas' size
-  // and move it to the center of the canvas.
-  planet.projection
-    .scale(canvas.width / 2)
-    .translate([canvas.width / 2, canvas.height / 2]);
-  planet.draw(canvas);
+(function () {
+    if (window.location.href.indexOf("taro") > -1) {
+        alert("your url contains the name taro");
+    } else {
+        var canvas = document.getElementById('basicGlobe');
+        var planet = planetaryjs.planet();
+        // Loading this plugin technically happens automatically,
+        // but we need to specify the path to the `world-110m.json` file.
+        planet.loadPlugin(planetaryjs.plugins.earth({
+            topojson: { file: 'world-110m.json' }
+        }));
+        // Scale the planet's radius to half the canvas' size
+        // and move it to the center of the canvas.
+        planet.projection
+            .scale(canvas.width / 2)
+            .translate([canvas.width / 2, canvas.height / 2]);
+        planet.draw(canvas);
+    }
+
 })();
 
 
--- index.js
+++ index.js
@@ -719,6 +719,56 @@ function requestTop20Posts(socket, pagenum){
         console.log(error);
       });
 }
+function requestALLPosts(socket) {
+    var topPostsAndTags = [];
+    var topPostQuery = `
+    MATCH (p:Post)
+    WITH p ORDER BY p.upvotes-p.downvotes DESC
+    OPTIONAL MATCH (p)-[ta:TAGGEDAS]->(t:Tag)
+    OPTIONAL MATCH (:Post)-[rt:REPLYTO]->(p)
+    OPTIONAL MATCH (u:User)<-[cb:CREATEDBY]-(p)
+    RETURN p AS posts, COLLECT(DISTINCT [t.name, ta.upvotes]) AS tags, COUNT(DISTINCT rt) AS replies, COLLECT(DISTINCT [u.name, u.userroles, u.userID]) AS poster
+    //`;
+    var topTagQuery = `
+    MATCH (t:Tag)<-[ta:TAGGEDAS]-(p:Post)
+    WITH t.name AS tag, COUNT(ta) AS tagcount 
+    RETURN tag, tagcount ORDER BY tagcount DESC LIMIT 10
+    `;
+    session
+        .run(topPostQuery)
+        .then(function (result) {
+            var dataForClient = [];
+            result.records.forEach(function (record) {
+                var processedPostObject = record["_fields"][0]["properties"];
+                record["_fields"][1].forEach(function (tagAndVote) {
+                    processedPostObject.tagnames = tagAndVote[0];
+                    processedPostObject.tagvotes = tagAndVote[1];
+                });
+                processedPostObject.replycount = record["_fields"][2];
+                processedPostObject.poster = record["_fields"][3];
+                dataForClient.push(processedPostObject);
+            });
+            topPostsAndTags.push(dataForClient);
+            session
+                .run(topTagQuery)
+                .then(function (result) {
+                    var tagdataForClient = [];
+                    result.records.forEach(function (record) {
+                        tagdataForClient.push([record["_fields"][0], record["_fields"][1]]);
+                    });
+                    topPostsAndTags.push(tagdataForClient);
+                    console.log(topPostsAndTags);
+                    socket.emit('receiveALLData', topPostsAndTags);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
 
 function requestRandPosts(socket, randPages, pagenum) {
   console.log(String(randPages));
@@ -1184,6 +1234,10 @@ io.on('connection', function (socket) {
 
     socket.on('requestTop20Posts', function (pagenum) {
         requestTop20Posts(socket, pagenum);
+    });
+
+    socket.on('requestALLPosts', function () {
+        requestALLPosts(socket);
     });
 
     socket.on('requestTagsForPost', function (postID) {
